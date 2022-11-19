762b1968e5d4939788e691c796ed41aaa17ba827
DARIEN DAVID MOLINA ROMERO <SMSS037322@ugb.edu.sv>
2022-11-18 22:33:03 -0600

    Agregar archivos de proyecto.

---
+++ Practica_semana_16/Controllers/Lucas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Practica_semana_16.Controllers
+{
+    public class Lucas : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2(string name, int age = 1)
+        {
+            ViewData["name"] = "Buenas tardes " + name;
+            ViewData["edad"] = "Tienes " + age + "anios";
+            return View();
+        }
+
+        public string? Parameters(string name, int age = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hola {name} tienes {age}, ya estás ruco xd");
+        }
+    }
+}
---
+++ Practica_semana_16/Data/Semana16_Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practica_semana_16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_semana_16.Data
+{
+    public class Semana16_Context : DbContext
+    {
+        public Semana16_Context(DbContextOptions<Semana16_Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
